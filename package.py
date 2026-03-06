baa461235840261a13306ae952ef344f2726ae5a
AquamarineCyan <64363680+AquamarineCyan@users.noreply.github.com>
2026-03-06 17:41:08 +0800

    Update package.py

--- package.py
+++ package.py
@@ -10,7 +10,7 @@ parser.add_argument('--nvidia', action='store_true')
 args = parser.parse_args()
 
 user_deps = [dist.metadata["Name"] for dist in importlib.metadata.distributions()]
-deps_all = list(paddlex.utils.deps.DEP_SPECS.keys())
+deps_all = list(paddlex.utils.deps.BASE_DEP_SPECS.keys())
 deps_need = [dep for dep in user_deps if dep in deps_all]
 
 cmd = [
